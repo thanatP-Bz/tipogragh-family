1906c6fb23ea3fb79f9ee870c3e2b08a10968734
Thanat <bztzn512@gmail.com>
2022-07-01 21:26:14 -0400

    fixed

--- src/Components/Event.js
+++ src/Components/Event.js
@@ -5,13 +5,13 @@ import EventImg from "../asserts/event.svg";
 const Event = () => {
   return (
     <section id="event" className="py-2">
-      <div className="container flex flex-col md:flex-row md:mx-auto md:justify-center my-10 space-y-0">
-        <div className="flex flex-col mb-10 md:w-3/4">
+      <div className="container flex flex-col md:flex-row md:mx-auto md:justify-center my-5 space-y-0">
+        <div className="flex flex-col mb-10 md:w-1/2">
           <div className="space-y-12 py-6">
             <h1 className=" text-3xl font-bold md:text-2xl md:text-left text-center">
               Welcome to my website
             </h1>
-            <p className="text-md text-center md:text-left  max-w-md">
+            <p className="text-md text-center items-center md:text-left  max-w-md">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias
               aliquam suscipit soluta dolorum ipsum! Fugit nihil cumque ipsum
               consectetur eligendi!
@@ -19,7 +19,7 @@ const Event = () => {
           </div>
         </div>
 
-        <div className="flex justify-center md:justify-start w-3/5 ">
+        <div className="flex justify-center md:justify-start w-1/2 mr-10">
           <img className="w-3/3" src={EventImg} alt="" />
         </div>
       </div>
--- src/Components/Feature.js
+++ src/Components/Feature.js
@@ -1,8 +1,29 @@
 import React from "react";
-import Support from "../asserts/support.svg";
+import SupportImg from "../asserts/support.svg";
 
 const Feature = () => {
-  return <section id="feature" className="py-2"></section>;
+  return (
+    <section id="feature" className="py-2">
+      <div className="container flex flex-col md:flex-row-reverse md:mx-auto md:justify-center my-5 space-y-0">
+        <div className="flex flex-col mb-10 md:w-1/2">
+          <div className="space-y-12 py-6 ml-16">
+            <h1 className=" text-3xl font-bold md:text-2xl md:text-left text-center">
+              Welcome to my website
+            </h1>
+            <p className="text-md text-center md:text-left  max-w-md">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias
+              aliquam suscipit soluta dolorum ipsum! Fugit nihil cumque ipsum
+              consectetur eligendi!
+            </p>
+          </div>
+        </div>
+
+        <div className="flex justify-center md:justify-start w-1/2  ">
+          <img className="w-3/3 h-3/4 " src={SupportImg} alt="" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Feature;
